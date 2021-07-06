491ce48ad3a92f7cd8771aa9327a2e9989438535
Eduardo <egbulsoni@gmail.com>
2021-07-06 04:42:58 -0300

    initial commit

---
+++ dictionaries/exercício_de_dicionário.py
@@ -0,0 +1,10 @@
+phonebook = {
+    'John': 1234
+    'Jack': 2345
+    'Jabb': 3456
+}
+
+if "Jack" in phonebook:
+    print("Jack is listed in the phonebook.")
+if "Jabb" not in phonebook:
+    print("Jabb is not listed in the phonebook.")
